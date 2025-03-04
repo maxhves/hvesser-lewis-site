ff42e19fdcbc78b8832e575ffa35ee8793d803d4
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-04 21:38:24 +1300

    Adjusted content container to use divs instead of borders.

--- src/components/ui/content-container.tsx
+++ src/components/ui/content-container.tsx
@@ -30,17 +30,21 @@ export function ContentContainerNew({
 }) {
   return (
     <div className={clsx(outerClassName, 'relative flex justify-center px-6 md:px-8')}>
-      <div className="absolute inset-y-0 left-1/2 z-0 w-px bg-stone-200 dark:bg-stone-800"></div>
-      <div
-        className={clsx(
-          className,
-          'z-10 flex w-full max-w-6xl flex-col border-x border-stone-200 dark:border-stone-800'
-        )}
-      >
+      {/* Center Line */}
+      <div className="absolute inset-y-0 left-1/2 z-0 w-px bg-stone-200 dark:bg-stone-800" />
+
+      <div className={clsx(className, 'relative flex w-full max-w-6xl flex-col')}>
+        {/* Left Line */}
+        <div className="absolute inset-y-0 left-0 w-px bg-stone-200 dark:bg-stone-800" />
+
+        {/* Right Line */}
+        <div className="absolute inset-y-0 right-0 w-px bg-stone-200 dark:bg-stone-800" />
+
         {children}
       </div>
     </div>
   )
 }
+
 
 //TODO: Refactor old container with new
