07318685c2ed2e667a35b8bdc73608f111fff56b
Maximilian Hvesser-Lewis <maxhvesser@gmail.com>
2025-03-05 22:17:50 +1300

    Improved project cards design.

--- src/app/home/components/projects/components/project-card.tsx
+++ src/app/home/components/projects/components/project-card.tsx
@@ -14,29 +14,64 @@ export default function ProjectCard({
   href: string
 }) {
   return (
-    <div className="w-full rounded-2xl border border-stone-200 bg-white dark:border-stone-800 dark:bg-stone-900 md:w-1/3">
-      <div className="w-full px-6">
-        <div className="w-full border-x border-stone-200 pb-8 pt-6 dark:border-stone-800">
-          <h1 className={clsx(dmSans.className, 'text-2xl font-medium text-stone-700 dark:text-stone-300')}>{title}</h1>
-          <p className="mt-4 whitespace-pre-wrap text-base font-normal text-stone-500">{description}</p>
+    <GridOutline className="flex flex-col gap-5 md:flex-row">
+      {/* Project Information */}
+      <GridOutline className="w-full md:w-2/5">
+        <div className="rounded-2xl border border-stone-200 bg-white dark:border-stone-800 dark:bg-stone-900">
+          <div className="w-full px-6">
+            <div className="w-full border-x border-stone-200 pb-8 pt-6 dark:border-stone-800">
+              <h1 className={clsx(dmSans.className, 'text-2xl font-medium text-stone-700 dark:text-stone-300')}>
+                {title}
+              </h1>
+              <p className="mt-4 whitespace-pre-wrap text-base font-normal text-stone-500">{description}</p>
+            </div>
+          </div>
+          <GridOutline className="bg-stone-50 px-6 dark:bg-stone-950/50" direction="vertical">
+            <GridOutline direction="horizontal">
+              <Link
+                className="relative flex h-10 w-full items-center justify-between rounded-full border border-stone-200 bg-white px-5 text-sm font-medium text-stone-700 hover:bg-stone-50 dark:border-stone-800 dark:bg-stone-900 dark:text-stone-300 dark:hover:bg-stone-950/50"
+                href={href}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Visit project website
+                <IconArrowRight className="size-4" />
+              </Link>
+            </GridOutline>
+          </GridOutline>
+          <div className="h-6 w-full px-6">
+            <div className="h-full w-full border-x border-stone-200 dark:border-stone-800" />
+          </div>
         </div>
-      </div>
-      <GridOutline className="bg-stone-50 px-6 dark:bg-stone-950/50" direction="vertical">
-        <GridOutline direction="horizontal">
-          <Link
-            className="relative flex h-10 w-full items-center justify-between rounded-full border border-stone-200 bg-white px-5 text-sm font-medium text-stone-700 hover:bg-stone-50 dark:border-stone-800 dark:bg-stone-900 dark:text-stone-300 dark:hover:bg-stone-950/50"
-            href={href}
-            rel="noreferrer"
-            target="_blank"
-          >
-            Visit project website
-            <IconArrowRight className="size-4" />
-          </Link>
-        </GridOutline>
       </GridOutline>
-      <div className="h-6 w-full px-6">
-        <div className="h-full w-full border-x border-stone-200 dark:border-stone-800" />
+
+      {/* Project Preview Image */}
+      <GridOutline className="w-full md:w-3/5">
+        <div className="h-80 border border-stone-200 bg-white p-2 dark:border-stone-800 dark:bg-stone-900 md:h-full">
+          <ImageTesting />
+        </div>
+      </GridOutline>
+    </GridOutline>
+  )
+}
+
+//region Image testing
+
+import desktopPreview from '../../../../../../public/images/find-the-menu-preview-desktop.webp'
+import mobilePreview from '../../../../../../public/images/find-the-menu-preview-mobile.webp'
+import Image from 'next/image'
+
+function ImageTesting() {
+  return (
+    <div className="group relative h-full w-full border border-stone-200 opacity-100 dark:border-stone-800 dark:opacity-90">
+      <div className="absolute inset-y-0 left-0 top-0 h-full w-full">
+        <Image className="object-cover object-top" src={desktopPreview} alt="Desktop preview" fill={true} />
+      </div>
+      <div className="absolute inset-y-0 right-0 top-0 h-full w-72 border-s border-stone-200">
+        <Image className="object-cover object-top" src={mobilePreview} alt="Desktop preview" fill={true} />
       </div>
     </div>
   )
 }
+
+//endregion
---
+++ src/app/home/components/projects/project-data.ts
@@ -0,0 +1,30 @@
+export const projectData = [
+  {
+    id: 1,
+    title: 'Find the Menu',
+    description:
+      'A platform with two core features. First, a mini-website creation tool, allowing businesses to easily showcase their menus on a single page.\n\nSecond, an AI-powered recommendation system that helps users find the best places to eat in their chosen area. Using a Large Language Model, it processes multiple parameters to deliver highly personalized suggestions.',
+    href: 'https://findthe.menu',
+  },
+  {
+    id: 2,
+    title: 'Visa Match',
+    description:
+      'Comprehensive immigration documentation for those looking to gain work experience abroad.\n\nThe site gathers and organizes official visa pathway information and condenses it into easy to understand guides.\n\nAn optional AI agent is available to provide guidance and answer questions about any visa program.',
+    href: 'https://visamatch.co',
+  },
+  {
+    id: 3,
+    title: 'Temperature Check',
+    description:
+      'Designed as part of a suite of health tools, this particular tool is designed for individuals who want to better understand their body temperature and identify readings that may fall outside the normal range or indicate a fever.\n\nSimply enter a temperature reading, and the tool will analyze it, providing insight into what it could mean for your health.',
+    href: 'https://temperature-check.com',
+  },
+  {
+    id: 4,
+    title: 'Crime Connoisseur',
+    description:
+      'A true crime podcast discovery platform designed to help users easily find and stream the latest true crime content.\n\nThe directory features powerful filtering options and prioritizes the most up-to-date shows and episodes, making content discovery effortless. Episodes can be streamed directly on the platform, providing a seamless listening experience without the need to leave the site.',
+    href: 'https://crimeconnoisseur.com',
+  },
+]
