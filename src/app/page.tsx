8216b3cb1fbf308fd8f52652fd1ef50b119974f1
Maximilian Hvesser-Lewis <46483870+maxhves@users.noreply.github.com>
2025-07-05 08:17:39 +0100

    Return notFound whilst site is under construction.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import Projects from '@/app/home/components/projects/projects'
 import Widgets from '@/app/home/components/widgets/widgets'
 
 export default function Home() {
-  return (
-    <main className="flex-grow">
-      <Introduction />
-      <Widgets />
-      <Expertise />
-      <Projects />
-    </main>
-  )
+  // return (
+  //   <main className="flex-grow">
+  //     <Introduction />
+  //     <Widgets />
+  //     <Expertise />
+  //     <Projects />
+  //   </main>
+  // )
+  return notFound()
 }
